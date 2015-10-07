752eba3d9ad1b7ec5ca40409c1a8941c81296d41
Parke Loyd <parke.loyd@gmail.com>
2015-10-07 10:20:40 -0600

    revamped stars 3d?

--- plotutils.py
+++ plotutils.py
@@ -8,8 +8,7 @@ Created on Fri May 30 17:15:27 2014
 import numpy as np
 import matplotlib as mplot
 import matplotlib.pyplot as plt
-#from mayavi import mlab
-from color.maps import true_temp
+import mypy.my_numpy as mnp
 
 dpi = 100
 fullwidth = 10.0
@@ -100,15 +99,30 @@ def pcolor_reg(x, y, z, **kw):
                **kw)
     plt.axis('tight')
 
-def errorpoly(x, y, yerr, fmt=None, ecolor=None, ealpha=0.5, **kw):
-    p = plt.plot(x,y,fmt, **kw)
+def errorpoly(x, y, yerr, fmt=None, ecolor=None, ealpha=0.5, ax=None, **kw):
+    if ax is None: ax = plt.gca()
+    p = ax.plot(x, y, **kw) if fmt is None else ax.plot(x, y, fmt, **kw)
     if len(yerr.shape) == 2:
         ylo = y - yerr[0,:]
         yhi = y + yerr[1,:]
     else:
         ylo, yhi = y - yerr, y + yerr
     if ecolor == None: ecolor = p[0].get_color()
-    f = plt.fill_between(x,ylo,yhi,color=ecolor,alpha=ealpha)
+
+    # deal with matplotlib sometimes not showing polygon when it extends beyond plot range
+    xlim = ax.get_xlim()
+    inrange = mnp.inranges(x, xlim)
+    if not np.all(inrange):
+        n = np.sum(inrange)
+        yends = np.interp(xlim, x, y)
+        yloends = np.interp(xlim, x, ylo)
+        yhiends = np.interp(xlim, x, yhi)
+        x = np.insert(x[inrange], [0, n], xlim)
+        y = np.insert(y[inrange], [0, n], yends)
+        ylo = np.insert(ylo[inrange], [0, n], yloends)
+        yhi = np.insert(yhi[inrange], [0, n], yhiends)
+
+    f = ax.fill_between(x,ylo,yhi,color=ecolor,alpha=ealpha)
     return p[0],f
 
 def onscreen_pres(mpl, screenwidth=1200):
@@ -131,89 +145,128 @@ def onscreen_pres(mpl, screenwidth=1200):
     fontsize = round(14 / (800.0 / screenwidth))
     mpl.rcParams['font.size'] = fontsize
 
-#def stars3d(ra, dec, dist, T=5000.0, r=1.0, labels='', view=None):
-#    """
-#    Make a 3D diagram of stars positions relative to the Sun, with
-#    semi-accurate colors and distances as desired. Coordinates must be in
-#    degrees.
-#
-#    Meant to be used with only a handful of stars.
-#    """
-#    n = len(ra)
-#    makearr = lambda v: np.array([v] * n) if np.isscalar(v) else v
-#    T, r, labels = map(makearr, (T, r, labels))
-#
-#    # add the sun
-#    ra, dec, dist = map(np.append, (ra, dec, dist), (0.0, 0.0, 0.0))
-#    r, T, labels = map(np.append, (r, T, labels), (1.0, 5780.0, 'Sun'))
-#
-#    # get xyz coordinates
-#    z = dist * np.sin(dec * np.pi / 180.0)
-#    h = dist * np.cos(dec * np.pi / 180.0)
-#    x = h * np.cos(ra * np.pi / 180.0)
-#    y = h * np.sin(ra * np.pi / 180.0)
-#
-#    # make figure
-#    fig = mlab.figure(bgcolor=(0,0,0), fgcolor=(1,1,1), size=(800,800))
-#
-#    # plot lines down to the dec=0 plane for all but the sun
-#    lines = []
-#    for x1, y1, z1 in zip(x, y, z)[:-1]:
-#        xx, yy, zz = [x1, x1], [y1, y1], [0.0, z1]
-#        line = mlab.plot3d(xx, yy, zz, color=(0.7,0.7,0.7), line_width=0.5,
-#                           figure=fig)
-#        lines.append(line)
-#
-#    # plot spheres
-#    r_factor = np.max(dist) / 30.0
-#    pts = mlab.quiver3d(x, y, z, r, r, r, scalars=T, mode='sphere',
-#                        scale_factor=r_factor, figure=fig, resolution=100)
-#    pts.glyph.color_mode = 'color_by_scalar'
-#
-#    # center the glyphs on the data point
-#    pts.glyph.glyph_source.glyph_source.center = [0, 0, 0]
-#
-#    # set a temperature colormap
-#    cmap = true_temp(T)
-#    pts.module_manager.scalar_lut_manager.lut.table = cmap
-#
-#    # set the camera view
-#    mlab.view(focalpoint=(0.0, 0.0, 0.0))
-#    if view is not None:
-#        mlab.view(*view)
-#
-#    ## add labels
-#    # unit vec to camera
-#    view = mlab.view()
-#    az, el = view[:2]
-#    hc = np.sin(el * np.pi / 180.0)
-#    xc = hc * np.cos(az * np.pi / 180.0)
-#    yc = hc * np.sin(az * np.pi / 180.0)
-#    zc = -np.cos(el * np.pi / 180.0)
-#
-#    # unit vec orthoganal to camera
-#    if xc**2 + yc**2 == 0.0:
-#        xoff = 1.0
-#        yoff = 0.0
-#        zoff = 0.0
-#    else:
-#        xoff = yc / np.sqrt(xc**2 + yc**2)
-#        yoff = np.sqrt(1.0 - xoff**2)
-#        zoff = 0.0
-#
-##    xoff, yoff, zoff = xc, yc, zc
-#
-#    # scale orthogonal vec by sphere size
-#    r_label = 1.0 * r_factor
-#    xoff, yoff, zoff = [r_label * v for v in [xoff, yoff, zoff]]
-#
-#    # plot labels
-#    size = r_factor / 2.0
-#    for xx, yy, zz, label in zip(x, y, z, labels):
-#        mlab.text3d(xx + xoff, yy + yoff, zz + zoff, label, figure=fig,
-#                    color=(0.5,0.5,0.5), scale=size)
-#
-#    mlab.draw()
-#    return fig
+
+def textBoxSize(txt, transformation=None, figure=None):
+    """Get the width and height of a text object's bounding box transformed to the desired coordinates. Defaults to
+    figure coordinates if transformation is None."""
+
+    fig= txt.get_figure() if figure is None else figure
+    if transformation is None:
+        transformation = fig.transFigure
+
+    coordConvert = transformation.inverted().transform
+    bboxDisp = txt.get_window_extent(fig.canvas.renderer)
+    bboxConv = coordConvert(bboxDisp)
+    w = bboxConv[1,0] - bboxConv[0,0]
+    h = bboxConv[1,1] - bboxConv[0,1]
+    return w, h
+
+
+def stars3d(ra, dec, dist, T=5000.0, r=1.0, labels='', view=None, size=(800,800), txt_scale=1.0):
+    """
+    Make a 3D diagram of stars positions relative to the Sun, with
+    semi-accurate colors and distances as desired. Coordinates must be in
+    degrees. Distance is assumed to be in pc (for axes labels).
+
+    Meant to be used with only a handful of stars.
+    """
+    from mayavi import mlab
+    from color.maps import true_temp
+
+    n = len(ra)
+    dec, ra = dec*np.pi/180.0, ra*np.pi/180.0
+    makearr = lambda v: np.array([v] * n) if np.isscalar(v) else v
+    T, r, labels = map(makearr, (T, r, labels))
+
+    # add the sun
+    ra, dec, dist = map(np.append, (ra, dec, dist), (0.0, 0.0, 0.0))
+    r, T, labels = map(np.append, (r, T, labels), (1.0, 5780.0, 'Sun'))
+
+    # get xyz coordinates
+    z = dist * np.sin(dec)
+    h = dist * np.cos(dec)
+    x = h * np.cos(ra)
+    y = h * np.sin(ra)
+
+    # make figure
+    fig = mlab.figure(bgcolor=(0,0,0), fgcolor=(1,1,1), size=size)
+
+    # plot lines down to the dec=0 plane for all but the sun
+    lines = []
+    for x1, y1, z1 in zip(x, y, z)[:-1]:
+       xx, yy, zz = [x1, x1], [y1, y1], [0.0, z1]
+       line = mlab.plot3d(xx, yy, zz, color=(0.7,0.7,0.7), line_width=0.5,
+                          figure=fig)
+       lines.append(line)
+
+    # plot spheres
+    r_factor = np.max(dist) / 30.0
+    pts = mlab.quiver3d(x, y, z, r, r, r, scalars=T, mode='sphere',
+                       scale_factor=r_factor, figure=fig, resolution=100)
+    pts.glyph.color_mode = 'color_by_scalar'
+
+    # center the glyphs on the data point
+    pts.glyph.glyph_source.glyph_source.center = [0, 0, 0]
+
+    # set a temperature colormap
+    cmap = true_temp(T)
+    pts.module_manager.scalar_lut_manager.lut.table = cmap
+
+    # set the camera view
+    mlab.view(focalpoint=(0.0, 0.0, 0.0), figure=fig)
+    if view is not None:
+       mlab.view(*view, figure=fig)
+
+    ## add labels
+    # unit vec to camera
+    view = mlab.view()
+    az, el = view[:2]
+    hc = np.sin(el * np.pi / 180.0)
+    xc = hc * np.cos(az * np.pi / 180.0)
+    yc = hc * np.sin(az * np.pi / 180.0)
+    zc = -np.cos(el * np.pi / 180.0)
+
+    # unit vec orthoganal to camera
+    if xc**2 + yc**2 == 0.0:
+       xoff = 1.0
+       yoff = 0.0
+       zoff = 0.0
+    else:
+       xoff = yc / np.sqrt(xc**2 + yc**2)
+       yoff = np.sqrt(1.0 - xoff**2)
+       zoff = 0.0
+
+    #    xoff, yoff, zoff = xc, yc, zc
+
+    # scale orthogonal vec by sphere size
+    r_label = 1.0 * r_factor
+    xoff, yoff, zoff = [r_label * v for v in [xoff, yoff, zoff]]
+
+    # plot labels
+    size = r_factor * txt_scale * 0.75
+    for xx, yy, zz, label in zip(x, y, z, labels):
+       mlab.text3d(xx + xoff, yy + yoff, zz + zoff, label, figure=fig,
+                   color=(1,1,1), scale=size)
+
+    ## add translucent dec=0 surface
+    n = 101
+    t = np.linspace(0.0, 2*np.pi, n)
+    r = np.max(dist * np.cos(dec))
+    x, y = r*np.cos(t), r*np.sin(t)
+    z = np.zeros(n+1)
+    x, y = [np.insert(a, 0, 0.0) for a in [x,y]]
+    triangles = [(0, i, i + 1) for i in range(1, n)]
+    mlab.triangular_mesh(x, y, z, triangles, color=(1,1,1), opacity=0.3, figure=fig)
+
+    ## add ra=0 line
+    line = mlab.plot3d([0, r], [0, 0], [0, 0], color=(1,1,1), line_width=1, figure=fig)
+    rtxt = '{:.1f} pc'.format(r)
+    orientation=np.array([180.0, 180.0, 0.0])
+    mlab.text3d(r, 0, 0, rtxt, figure=fig, scale=size*1.25, orient_to_camera=False, orientation=orientation)
+
+    if view is not None:
+       mlab.view(*view, figure=fig)
+
+    return fig
 
 
