636777c0a4f252c572e041e991ecb68daac6ab01
parke <parke.loyd@gmail.com>
2018-10-16 14:10:34 -0400

    roman numerating

--- pubutils.py
+++ pubutils.py
@@ -366,3 +366,15 @@ def number_check(ms_path):
             snippet = line[i0:i1]
             snippet = '{}: '.format(i) + snippet
             print snippet
+
+
+_num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+            (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+def num2roman(num):
+    roman = ''
+    while num > 0:
+        for i, r in _num_map:
+            while num >= i:
+                roman += r
+                num -= i
+    return roman
